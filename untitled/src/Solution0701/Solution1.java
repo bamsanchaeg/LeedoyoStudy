8d9b7eea6ca51955c2284870b192e5e7c1ce21ae
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2024-07-01 09:42:10 +0900

    0630

---
+++ untitled/src/Solution0701/Solution1.java
@@ -0,0 +1,12 @@
+package Solution0701;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+public class Solution1 {
+    public static void main(String[] args) {
+        System.out.println("문자열을 입력하세요");
+        BufferedReader alphabetReader = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+}
