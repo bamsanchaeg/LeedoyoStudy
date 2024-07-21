ca4c58e3c37a114f42e6575aaa5f3d832dbf5d4b
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2024-07-21 22:15:17 +0900

    0721 스터디
    HashMap 구조와 여러 API 공부가 필요함. Static 및 여러 기본 메서드에 대한 복습 및 자료구조에 대한 이해가 필요할 것 같음. Solution5의 경우 내 힘으로 푼 경우가 거의 없으니 복습 철저히 할 것.

--- src/main/java/Solution0714/Solution1.java
+++ src/main/java/Solution0714/Solution1.java
@@ -13,16 +13,16 @@ public class Solution1 {
     public static void main(String[] args) throws IOException {
         int answer = 0;
 
-        for(int i =1; i*i<=n; i++){
-            if(n%i == 0){
-                answer +=2;
-                if(i*i == n){
-                    answer--;
-                }
-            }
-
-        }
-        return answer;
+//        for(int i =1; i*i<=n; i++){
+//            if(n%i == 0){
+//                answer +=2;
+//                if(i*i == n){
+//                    answer--;
+//                }
+//            }
+//
+//        }
+//        return answer;
 
     }
 }
